2c2c15ba9605eb066a007f2fe362aa81cccc0a05
Leonel Marcos <mfjleonel@gmail.com>
2021-03-10 08:39:03 -0300

    Code refactory better efficiency and readability
    Did some changes on how it works, speacilly by making the main variables
    global, and setting specific functions for specific tasks, we still
    have our "game" function, which is the main and runs a while loop, and
    the "get_args", but included a "show" which was a merge of the block of
    code that checked the user platform and clean screen with the
    right command, and the block that showed remaining attempts, the found
    and to be found letters. And a "gen_word" that generates the word.

--- hangman.py
+++ hangman.py
@@ -1,76 +1,83 @@
-import argparse, random, subprocess, sys
+import argparse
+import random
+import subprocess
+import sys
 
-words = ['farinha', 'pássaro', 'igreja', 'maçã', 'banana', 'laranja']
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-l', '--lenght', default='*',
+                        help="Escolhe o tamanho das palavras")
+    return parser.parse_args()
 
 
-def game(word):
-    preserved_word = word
-    list_word = ['_' for a in word]
-    life = 5
+def gen_word():
+    global args
+    choosen_word = random.choice(words)
+
+    if args.lenght != '*':
+        try:
+            lenght = int(args.lenght)
+            if lenght > 12 or lenght < 4:
+                raise(ValueError)
+            else:
+                while len(choosen_word) != lenght:
+                    choosen_word = random.choice(words)
+
+        except ValueError:
+            print("> Deve-se passar um número entre 4 e 12 como argumento para 'lenght'.")
+    return choosen_word.lower()
+
+
+def show():
+    if sys.platform == 'win32':
+        subprocess.call("cls", shell=True)
+    else:
+        subprocess.call("clear", shell=True)
+
+    life_hash = ['#' for l in range(life)]
+    print(f"Tentativas restantes: {' '.join(life_hash)} \n")
+    print(f"{' '.join(list_word)} \n")
+
+
+def game():
+    global life
+    global list_word
+
+    word_alt = word.lower()
     guess = ''
-
-    while ''.join(list_word) != preserved_word and life > 0:
-        if sys.platform =='win32':
-            subprocess.call("cls", shell=True)
-        else:
-            subprocess.call("clear", shell=True)
-        
-        life_hash = ['#' for l in range(life)]
-        print(word)
-        print(f"Tentativas restantes: {' '.join(life_hash)} \n")
-        print(f"{' '.join(list_word)}")
-
+    while ''.join(list_word) != word and life > 0:
+        show()
         guess = input("> Digite uma letra, ou mais de uma para tentar advinhar a palavra. Digite 'quit' a qualquer momento, para sair. \n")
         guess = guess.lower()
 
         if guess == 'quit':
             break
-        elif len(guess)>1 and guess!='quit':
-            if guess == preserved_word:
-                print("Parabéns!")
+        elif len(guess) > 1 and guess != 'quit':
+            if guess == word:
+                list_word = [l for l in word]
+                break
             else:
                 life = 0
-            
+
         else:
-            if guess in word:
-                for char in word:
+            if guess in word_alt:
+                for char in word_alt:
                     if char == guess:
-                        list_word[word.index(char)] = char
-                        word = word.replace(char,'X', 1)
+                        list_word[word_alt.index(char)] = char
+                        word_alt = word_alt.replace(char, 'X', 1)
             else:
                 life -= 1
+    
+    show()
     if life == 0:
-        
         print("Você perdeu! Boa sorte na próxima.")
     else:
         print("Parabéns, você conseguiu!")
 
 
-def get_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-l', '--lenght', default='*', help="Escolhe o tamanho das palavras")
-    return parser.parse_args()
-
+words = ['farinha', 'pássaro', 'igreja', 'maçã', 'banana', 'laranja', 'pera', 'mamão', 'graça', 'Deus', 'amor', 'vídeo','espada', 'livro','paralelo', 'linha', 'tempo', 'vida','triste']
+life = 5
 args = get_args()
-
-choosen_word = random.choice(words)
-
-if args.lenght != '*':
-    try:
-        lenght = int(args.lenght)
-        if lenght > 12 or lenght < 4:
-            raise(ValueError)
-        else:
-            while len(choosen_word) != lenght:
-                choosen_word = random.choice(words)
-
-    except ValueError:
-        print("> Deve-se passar um número entre 4 e 12 como argumento para 'lenght'.")
-
-print(choosen_word)
-game(choosen_word)
-
-        
-
-
-
+word = gen_word()
+list_word = ['_' for a in word]
+game()
